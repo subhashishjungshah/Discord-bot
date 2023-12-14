137552fa6c9ef26e482b159ae88476c58772ae07
subhashishjungshah <subhashishjungshah@gmail.com>
2023-12-14 20:20:37 +0545

    setting up discord bot setup

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+const fs = require("node:fs");
+const path = require("node:path");
+const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
+
+const token = process.env.TOKEN;
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (readyClient) => {
+  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(token);
+
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, "commands");
+console.log(foldersPath);
